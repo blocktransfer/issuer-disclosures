8d90d5be45112721a96e00e4c77d4560a4d18fb5
John Wooten <hello@blocktransfer.dev>
2023-09-06 08:50:17 -0400

    Update getFooter.js

--- getFooter.js
+++ getFooter.js
@@ -1,15 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   let footerPath = "/footer.html";
-  const pathSplit = window.location.pathname.split("/");
-  if (pathSplit.length > 2) {
+  if (window.location.pathname.split("/").length > 2) {
     footerPath = "../footer.html";
   }
   fetch(footerPath)
     .then(response => {
-      if (!response.ok) {
-        console.log("Fetch failed:", response.status, response.statusText);
-        return null;
-      }
       return response.text();
     })
     .then(data => document.body.insertAdjacentHTML("afterend", data))

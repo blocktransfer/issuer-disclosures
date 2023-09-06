fd5bb10dba228ee83e9ac69f560eeae1edcd9d65
John Wooten <hello@blocktransfer.dev>
2023-09-06 06:10:57 -0400

    Footer

--- getFooter.js
+++ getFooter.js
@@ -5,9 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
     footerPath = "../footer.html";
     document.querySelector("img[alt='logo']").src = "../logo-white.png";
   }
+  console.log("Fetching footer from:", footerPath);
   fetch(footerPath)
-    .then(response => response.text())
+    .then(response => {
+      if (!response.ok) {
+        console.log("Fetch failed:", response.status, response.statusText);
+        return null;
+      }
+      return response.text();
+    })
     .then(data => {
-      document.body.insertAdjacentHTML("beforeend", data);
+      if (data) {
+        document.body.insertAdjacentHTML("beforeend", data);
+      }
+    })
+    .catch(error => {
+      console.log("Fetch error:", error);
     });
 });

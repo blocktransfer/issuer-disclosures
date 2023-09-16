c5e30c3feae89a6fab039d42a2bb6fefef97bd5d
John Wooten <hello@blocktransfer.dev>
2023-09-16 05:56:37 -0400

    Update loadGetNumOutstanding.js

--- loadGetNumOutstanding.js
+++ loadGetNumOutstanding.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("getNumOutstanding").addEventListener("click", function(event) {
     event.preventDefault();
-    const securityType = event.target.getAttribute("security-code");
+    const securityType = event.target.getAttribute("data-code");
     event.target.textContent = "Loading..."
 	getNumOutstanding(securityType);
   });
@@ -10,21 +10,19 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("https://api.blocktransfer.com/getNumOutstanding/" + code)
       .then(response => response.json())
       .then(data => {
-        updateOutstandingField(securityType, data.outstanding);
+        updateOutstandingField(code, data.outstanding);
       })
       .catch(error => {
         console.error("Error:", error);
-        updateOutstandingField(securityType, "Error");
+        updateOutstandingField(code, 0);
       });
   }
 
   function updateOutstandingField(securityType, num) {
-    if (securityType === "ORD") {
-      document.getElementById("ORD_out").textContent = num;
-    } else if (securityType === "PFD") {
-      document.getElementById("PFD_out").textContent = num;
-    } else if (securityType === "Error") {
-	  document.getElementById("ORD_out").textContent = "90000000"; //TEMP
+    if (num) {
+      document.getElementById(code).textContent = num;
+    } else {
+	  document.getElementById(code).textContent = "Failed to load";
 	}
   }
 });

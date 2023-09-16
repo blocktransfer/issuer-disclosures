4f4c7ddb94fee84ca04be5ec09d5e1ba62c1fe7d
John Wooten <hello@blocktransfer.dev>
2023-09-16 06:04:22 -0400

    Update loadGetNumOutstanding.js

--- loadGetNumOutstanding.js
+++ loadGetNumOutstanding.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("getNumOutstanding").addEventListener("click", function(event) {
     event.preventDefault();
-    const securityType = event.target.getAttribute("asset-code");
     event.target.textContent = "Loading..."
-	getNumOutstanding(securityType);
+	const code = event.target.getAttribute("asset-code");
+    getNumOutstanding(code);
   });
   
   function getNumOutstanding(code) {

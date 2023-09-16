715f529931fec2f4ec9d9c2f7a33c34c8ce0ba40
John Wooten <hello@blocktransfer.dev>
2023-09-16 05:38:01 -0400

    Inherit getNumLocalShares API call
    Potentially do the arithmatic locally

---
+++ loadGetNumOutstanding.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("getNumOutstanding").addEventListener("click", function(event) {
+    event.preventDefault();
+    const securityType = event.target.getAttribute("data-security-type");
+    getNumOutstanding(securityType);
+  });
+
+  function getNumOutstanding(securityType) {
+    const code = "1984803" + securityType;
+    fetch("https://api.blocktransfer.com/getNumOutstanding/" + code)
+      .then(response => response.json())
+      .then(data => {
+        if (securityType === "ORD") {
+          document.getElementById("ORD_out").textContent = data.outstanding;
+        } else if (securityType === "PFD") {
+          document.getElementById("PFD_out").textContent = data.outstanding;
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }
+});

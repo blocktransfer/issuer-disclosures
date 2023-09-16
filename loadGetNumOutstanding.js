c5b1d0989655c53c6618d1704f942ca94a6a8f24
John Wooten <hello@blocktransfer.dev>
2023-09-16 06:56:19 -0400

    Update loadGetNumOutstanding.js

--- loadGetNumOutstanding.js
+++ loadGetNumOutstanding.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("getNumOutstanding").addEventListener("click", function(event) {
     event.preventDefault();
-	const code = event.target.getAttribute("asset-code");
-	setOutstanding(event, "Loading...")
+    const code = event.target.getAttribute("asset-code");
+    setOutstanding(event, "Loading...");
     getNumOutstanding(code, event);
   });
   
   function getNumOutstanding(code, event) {
-	fetch("https://api.blocktransfer.com/getNumOutstanding/" + code)
+    fetch("https://api.blocktransfer.com/getNumOutstanding/" + code)
       .then(response => response.json())
-      .then(data => setOutstanding(event, data.outstanding);)
+      .then(data => {
+        setOutstanding(event, data.outstanding); // Call setOutstanding when data is available
+      })
       .catch(error => {
-		setOutstanding(event, "Failed to load");
+        setOutstanding(event, "Failed to load");
       });
   }
   
   function setOutstanding(event, val) {
-	console.log("val in setOutstanding:", val); // Debug statement
-	event.target.textContent = val;
-	console.log("setOutstanding called - ", val)
+    console.log("val in setOutstanding:", val); // Debug statement
+    event.target.textContent = val;
+    console.log("setOutstanding called - ", val);
   }
 });

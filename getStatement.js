60b99c5ce04da52c84bdedf31d31a033883d2727
John Wooten <hello@blocktransfer.dev>
2023-10-04 05:03:43 -0400

    Update getStatement.js

--- getStatement.js
+++ getStatement.js
@@ -47,6 +47,6 @@ fetch(parent)
     }
   })
   .catch(error => {
-    console.log("Error finding statement: no", error);
+    console.log("Error finding statement: no", error.text());
     window.location.href = "https://www.blocktransfer.com/404?utm_source=issuersInfo404&utm_medium=error";
   });
